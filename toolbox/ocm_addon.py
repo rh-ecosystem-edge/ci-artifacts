a60266e7d3db8ded1b3fd1c778cb844bbc04525d
Sagi Dayan <sagidayan@gmail.com>
2022-05-25 12:51:00 +0300

    OSDE2E addon test tweaks
    - on integration will not install RHODS.
    - Updated addon deployment to use the new `nvidia-gpu-addon`.
    - Added ability to pass addon parameters for install
    - updated README with new ocm_addon install param `ocm_addon_params`

--- toolbox/ocm_addon.py
+++ toolbox/ocm_addon.py
@@ -9,7 +9,7 @@ class OCMAddon:
     Commands for managing OCM addons
     """
     @staticmethod
-    def install(ocm_refresh_token, ocm_cluster_id, ocm_url, ocm_addon_id, wait_for_ready_state=False):
+    def install(ocm_refresh_token, ocm_cluster_id, ocm_url, ocm_addon_id, ocm_addon_params=[], wait_for_ready_state=False):
         """
         Installs an OCM addon
 
@@ -18,13 +18,16 @@ class OCMAddon:
             ocm_cluster_id: Cluster ID from OCM's POV
             ocm_url: Used to determine environment
             ocm_addon_id: the addon id to install. (such as `managed-odh`, `gpu-operator-certified-addon` etc.)
+            ocm_addon_params: Optional. list of dicts i.e [{"id":"something","value":"something"},{"id":"something_else","value":"we-get-it"}]
             wait_for_ready_state: Optional. If true will cause the role to wait until addon reports ready state. (Can time out)
+
         """
         opt = {
             "ocm_addon_id": ocm_addon_id,
             "ocm_cluster_id": ocm_cluster_id,
             "ocm_url": ocm_url,
             "ocm_refresh_token": ocm_refresh_token,
+            "ocm_addon_params": ocm_addon_params,
             "wait_for_ready_state": wait_for_ready_state,
         }
         return PlaybookRun("ocm_install_addon", opt)
